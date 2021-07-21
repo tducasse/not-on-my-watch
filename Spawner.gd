bb51932859d74644f08e52d26e856208f7ed10b1
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2021-07-21 23:16:29 +1000

    spawn multiple

--- Spawner.gd
+++ Spawner.gd
@@ -5,10 +5,33 @@ onready var timer = $Timer
 
 export(NodePath) var target_path
 
-var time = 2.0
+var time = 0.5
 
+# 512x288
 var random_pos = [
-	[100, 100]
+	[530, 140],
+	[530, 180],
+	[530, 210],
+	[530, 240],
+	[530, 250],
+	[530, 260],
+	[530, 280],
+	[-30, 140],
+	[-30, 180],
+	[-30, 210],
+	[-30, 250],
+	[-30, 260],
+	[-30, 270],
+	[-30, 280],
+	[0, 300],
+	[100, 300],
+	[150, 300],
+	[200, 300],
+	[256, 300],
+	[300, 300],
+	[350, 300],
+	[412, 300],
+	[512, 300],
 ]
 
 var target_pos = null
@@ -17,6 +40,7 @@ var target_pos = null
 func _ready():
 	randomize()
 	target_pos = get_node(target_path).position
+	timer.wait_time = time
 	timer.start()
 
 
